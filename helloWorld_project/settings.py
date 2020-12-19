25877a53ccadfad5f4dffbf83623e99f6dd67313
Anirudh <ranirudh702@gmail.com>
2020-12-19 15:49:30 -0800

    updates for heroku

--- helloWorld_project/settings.py
+++ helloWorld_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*^shvy-j8m1(_0#(lxsr0os8=tfl9&xa-zv8zglcc#_rk#f=tr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
